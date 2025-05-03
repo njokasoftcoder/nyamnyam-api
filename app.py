36032bf4d4c0a92ff86d8e98ba02a2feabb262fa
njokasoftcoder <njomu2018@gmail.com>
2025-05-03 18:38:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -79,7 +79,6 @@ def show_logo():
     </html>
     '''
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)  # Allow LAN access
 
