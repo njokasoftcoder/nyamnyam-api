d71fc5a0ef85f586cc27c77239351cde064cd632
njokasoftcoder <njomu2018@gmail.com>
2025-06-18 12:40:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -87,5 +87,6 @@ def predict():
     except Exception as e:
         return jsonify({'error': str(e)}), 400
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
+
